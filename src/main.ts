0dc3ce0ec6b641c03c6388d689503efb65844963
alexeyinn <alexey.i.nn@gmail.com>
2022-05-31 11:44:04 +0300

    code splitting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-//2-17
+//2-19
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Можем получать любого вида объекты,
+      // отдаем только стандартного вида
+      whitelist: true,
+      // Не можем получать отличного от вида
+      // от валидированного DTO
+      forbidNonWhitelisted: true,
+      // Если получается, приводит типы получаемых
+      // данных, к указанными нами через TS
+      transform: true,
+    })
+  );
   await app.listen(3000);
 }
 
