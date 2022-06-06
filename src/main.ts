f7968eaac86c59e09eea6abb79b93a9a09ad74b1
Alexey Ilin <alexey.i.nn@gmail.com>
2022-06-06 10:20:02 +0300

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 import { ApiKeyGuard } from "./common/guards/api-key.guard";
 import { TimeoutInterceptor } from "./common/interceptors/timeout.interceptor";
 import { WrapResponseInterceptor } from "./common/interceptors/wrap-response.interceptor";
-//2-60
+//2-61
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -29,6 +30,15 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor()
   );
+
+  const options = new DocumentBuilder()
+    .setTitle("Coffee-app")
+    .setDescription("Coffee application")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 
